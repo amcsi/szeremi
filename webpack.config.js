0979e7239ada8e788ccb99ab6c3f88ff8b75b4fe
Attila Szeremi <attila@szeremi.org>
2015-11-25 20:48:47 +0000

    Move built asserts to "build" folder

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
         ]
     },
     output: {
-        path: path.join(__dirname, "public/assets"),
+        path: path.join(__dirname, "public/build"),
         publicPath: "public/",
         filename: "app.js"
     },
