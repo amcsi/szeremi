10d28a8239a768ba4ae474be3945fddbe8054cf1
Attila Szeremi <attila@szeremi.org>
2015-12-30 21:36:38 +0000

    Allow hot reloading to keep language when changing translations

--- app/core/translator.js
+++ app/core/translator.js
@@ -1,14 +1,15 @@
 /**
  * Translator interface.
  */
-import i18next from 'i18next/lib/index';
+import i18next from './translator/i18next';
 
 import resources from '../constants/lang/index';
 
+const lng = i18next.language;
+
 i18next.init({
+  lng,
   resources,
-  lng: 'en',
-  fallbackLng: 'en',
 });
 
 export default i18next;
---
+++ app/core/translator/i18next.js
@@ -0,0 +1,8 @@
+import i18next from 'i18next/lib/index';
+
+i18next.init({
+  lng: 'en',
+  fallbackLng: 'en',
+});
+
+export default i18next;
