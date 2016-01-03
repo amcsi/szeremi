d5ce174290683395c36844e3af328ee9345377ec
Attila Szeremi <attila@szeremi.org>
2016-01-03 19:46:34 +0000

    Change pure functional components back to React Components
    Pure components are unfortunately not yet supported in
    babel-plugin-react-transform. gaearon/babel-plugin-react-transform#57

--- app/components/organisms/HeaderSection.jsx
+++ app/components/organisms/HeaderSection.jsx
@@ -4,22 +4,31 @@ import { translate } from 'react-i18next/lib/index';
 import {Navbar, Nav} from 'react-bootstrap';
 import {Link} from 'react-router';
 
-function HeaderSection({t}) {
-  return (
-    <div>
-      <Navbar>
-        <Navbar.Header>
-          <Navbar.Brand>
-            Szeremi
-          </Navbar.Brand>
-        </Navbar.Header>
-        <Nav pullRight>
-          <li><Link to="/">{t('home')}</Link></li>
-          <li><Link to="/resume">{t('resumé')}</Link></li>
-        </Nav>
-      </Navbar>
-    </div>
-  );
-}
+const HeaderSection = React.createClass({
+
+  propTypes: {
+    t: React.PropTypes.func.isRequired,
+  },
+
+  render() {
+    const {t} = this.props;
+
+    return (
+      <div>
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              Szeremi
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Nav pullRight>
+            <li><Link to="/">{t('home')}</Link></li>
+            <li><Link to="/resume">{t('resumé')}</Link></li>
+          </Nav>
+        </Navbar>
+      </div>
+    );
+  },
+});
 
 export default translate(['translation'])(HeaderSection);
--- app/components/pages/HomePage.jsx
+++ app/components/pages/HomePage.jsx
@@ -4,16 +4,25 @@ import { translate } from 'react-i18next/lib/index';
 import HomePageLanguageSelector from '../molecules/HomePageLanguageSelector';
 import HeaderSection from '../organisms/HeaderSection';
 
-function HomePage({t}) {
-  return (
-    <div>
-      <HeaderSection/>
-      <main className="text-center">
-        <h1>{t('homepage.title')}</h1>
-        <HomePageLanguageSelector/>
-      </main>
-    </div>
-  );
-}
+const HomePage = React.createClass({
+
+  propTypes: {
+    t: React.PropTypes.func.isRequired,
+  },
+
+  render() {
+    const {t} = this.props;
+
+    return (
+      <div>
+        <HeaderSection/>
+        <main className="text-center">
+          <h1>{t('homepage.title')}</h1>
+          <HomePageLanguageSelector/>
+        </main>
+      </div>
+    );
+  },
+});
 
 export default translate(['translation'])(HomePage);
