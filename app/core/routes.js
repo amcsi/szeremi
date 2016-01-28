edeec13f1513d98b022898eb2b8392244b79754b
Attila Szeremi <attila@szeremi.org>
2016-01-28 22:23:31 +0000

    Fix routes that weren't working with an IndexRoute

--- app/core/routes.js
+++ app/core/routes.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import HomePage from '../components/pages/HomePage';
 import Resume from '../components/pages/Resume';
-import {Router, Route} from 'react-router';
+import {Router, Route, IndexRoute} from 'react-router';
 
 const routes = (
   <Router>
-    <Route path="/" component={HomePage}>
+    <Route path="/">
+      <IndexRoute component={HomePage} />
       <Route path="resume" component={Resume}/>
     </Route>
   </Router>
