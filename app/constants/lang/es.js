9c68d162e633a7d810d75bde8edafbc19ca7117a
Attila Szeremi <attila@szeremi.org>
2016-04-03 17:02:13 +0100

    Add Spanish translation for About Me page

--- app/constants/lang/es.js
+++ app/constants/lang/es.js
@@ -1,6 +1,28 @@
 export default {
   translation: {
     about: 'Sobre mi',
+    aboutPage: {
+      p1: 'He estado facinado de las computadoras desde tener 4 años cuando mi família era dueño' +
+      '  un Macintosh Classic que yo también usaba. Mi experiencia creativa empezó cuando tenía' +
+      ' alrededor de 10 años cuando hacía videos animados con Microsoft Office \'97 similar a' +
+      ' videos Flash, y también cuando construía los niveles propios de Duke Nukem 3D con la' +
+      ' herramienta de editar niveles.',
+      p2: 'Después de acabar la escuela secundaria, eché de perder de favorecer la universidad y' +
+      ' profeción más prestigioso en vez de seguir la pasión. Cuando no salió bien, sin embargo,' +
+      ' pude seguir la dirección correcta después de irse de los estudios temprano y empezar un' +
+      ' curso pagado de programación PHP. Fue eso que empezó mi camino de volverse a un' +
+      ' programador web que encontraría la alegría en hacer aplicaciones web para la' +
+      ' mejoramiendo de la humanidad ;)',
+      p3: 'Ahora vivo en Londres trabajando en LifeWorks, desarollando una aplicación web red' +
+      ' social para otras empresas donde las colegas pueden darse reconocimentos por cualquieres' +
+      ' contribuciones positivas, dar las colegas motivación de contribuir már para una' +
+      ' experiencia más divertido y alegre.',
+      p4: 'Soy un fan de anime y geek, y uno que le gusta salir a beber y perder el tiempo.' +
+      ' Relacionado con el trabajo pretendo estar puntual cuando discutir ticketes, y esfuerzo' +
+      ' para comentar el código para que ortos puedan enterder lo que pasa. Estoy muy abierto a' +
+      ' las nuevas tecnologías y siempre sigo el ritmo de las tendencias leyendo muchos' +
+      ' artículos blogs y Reddit relacionado con desarollo web.',
+    },
     blog: 'Blog',
     countryCodes: {
       ES: 'España',
