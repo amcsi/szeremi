20556e966e03aee6fc43019d67318f8a0bae0f29
Attila Szeremi <attila.szeremi@workangel.com>
2017-03-11 19:17:01 +0100

    Improve Spanish about-me

--- app/constants/lang/es.js
+++ app/constants/lang/es.js
@@ -2,26 +2,21 @@ export default {
   translation: {
     about: 'Sobre mi',
     aboutPage: {
-      p1: 'He estado facinado de las computadoras desde tener 4 años cuando mi família era dueño' +
-      '  un Macintosh Classic que yo también usaba. Mi experiencia creativa empezó cuando tenía' +
-      ' alrededor de 10 años cuando hacía videos animados con Microsoft Office \'97 similar a' +
-      ' videos Flash, y también cuando construía los niveles propios de Duke Nukem 3D con la' +
-      ' herramienta de editar niveles.',
-      p2: 'Después de acabar la escuela secundaria, eché de perder de favorecer la universidad y' +
-      ' profeción más prestigioso en vez de seguir la pasión. Cuando no salió bien, sin embargo,' +
-      ' pude seguir la dirección correcta después de irse de los estudios temprano y empezar un' +
-      ' curso pagado de programación PHP. Fue eso que empezó mi camino de volverse a un' +
-      ' programador web que encontraría la alegría en hacer aplicaciones web para la' +
-      ' mejoramiendo de la humanidad ;)',
-      p3: 'Ahora vivo en Londres trabajando en LifeWorks, desarollando una aplicación web red' +
-      ' social para otras empresas donde las colegas pueden darse reconocimentos por cualquieres' +
-      ' contribuciones positivas, dar las colegas motivación de contribuir már para una' +
-      ' experiencia más divertido y alegre.',
-      p4: 'Soy un fan de anime y geek, y uno que le gusta salir a beber y perder el tiempo.' +
-      ' Relacionado con el trabajo pretendo estar puntual cuando discutir ticketes, y esfuerzo' +
-      ' para comentar el código para que ortos puedan enterder lo que pasa. Estoy muy abierto a' +
-      ' las nuevas tecnologías y siempre sigo el ritmo de las tendencias leyendo muchos' +
-      ' artículos blogs y Reddit relacionado con desarollo web.',
+      p1: `
+        Me facianaban las computadoras desde tener 4 años cuando mi familia consigieron 
+        un Mahintosh Classic, lo que yo también usaba. Empezé las obras creativas de la informatica teniendo 10 años
+        cuando jugaba con Microsoft PowerPoint '97 creando obras parecido como videos flash, y cuando estaba construyendo
+        mis niveles propios para Duke Nukem 3D con su herramiento para crear niveles.
+      `,
+      p2: `
+        Después de acabar la escuela secundaria, comité un error de que ir a una universisad estudiar un gremio prestigioso en vez de seguir mis pasiones. Sin embargo cuando me di cuenta de que no me pasó bien, dejé mis estudios temprano y asistí un curso pagado por programación en PHP. Ésto empezó mi camino de volver a un programador web, él que encontraría la alegría por construir aplicaciones web para la mejoramiento de la humanidad ;)
+      `,
+      p3: `
+        Ahora vivo en Londres trabajando por LifeWorks desarollando una aplicación de red social para empresas. Allí los compañeros de tranajon pueden darse reconocimientos por cualquiér contribuciones positivas las que harían dentro de la empresa. Esto da los compañeros de trabajo motivación para contributar más és para hacer el trabajo una experiencia más divertido y alegre.
+      `,
+      p4: `
+          Soy un geek y fan de anime, y también me gusta ir para beber y hacer el tonto. Cuando trabajo, trado de estar puntual cuando discuto tickets, y pongo esfuerzo para dejar comentos en el código para que el equipo pueda entender qué pasa. Me interesa nuevas tecnologías, y siempre trato de seguir las nuevas tendencias por leer muchos blogs y subreddits de la programación.
+      `,
     },
     blog: 'Blog',
     countryCodes: {
