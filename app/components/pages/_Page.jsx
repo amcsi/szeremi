c8b36bd3c140bbfb0f68d9f957dd6847d7b25591
Attila Szeremi <attila@szeremi.org>
2019-09-17 13:25:25 +0200

    Show home page vertically centered if possible

--- app/components/pages/_Page.jsx
+++ app/components/pages/_Page.jsx
@@ -7,10 +7,19 @@ import { hot } from 'react-hot-loader';
 class Page extends React.Component {
   render() {
     return (
-      <div>
+      <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
         <Header />
 
-        <div className="container">
+        <div
+          className="container"
+          style={{
+            flexGrow: 1,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
           { this.props.children }
         </div>
 
