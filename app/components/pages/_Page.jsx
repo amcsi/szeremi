90b670ddc46749735f3c83f3298a82079e0935fb
Attila Szeremi <attila@szeremi.org>
2019-09-17 11:45:31 +0200

    Allow hot reloading outside of page content...
    https://github.com/gatsbyjs/gatsby/issues/8237#issuecomment-422213443
    Though according to someone on discord, hot reloading should have been
    working out of the box.
    Also, not sure if this change has any consequences.

--- app/components/pages/_Page.jsx
+++ app/components/pages/_Page.jsx
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import Header from '../organisms/Header';
 import Footer from '../organisms/Footer';
+import { hot } from 'react-hot-loader';
 
 class Page extends React.Component {
   render() {
@@ -21,4 +22,4 @@ Page.propTypes = {
   children: PropTypes.node,
 };
 
-export default (Page);
+export default hot(module)(Page);
