a19c53a9a16104788fde82905d3993d6514b61ce
Attila Szeremi <attila@szeremi.org>
2019-09-17 14:25:17 +0200

    Fix serve command once again

--- bin/run_production.sh
+++ bin/run_production.sh
@@ -6,4 +6,4 @@ set -e
 # Need to build in docker run step to take env vars into account for e.g. Google Analytics
 npm run build
 
-npm run serve --port 8080 --host 0.0.0.0
+npm run serve -- --port 8080 --host 0.0.0.0
